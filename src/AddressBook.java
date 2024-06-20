134f295ec26e4701daf2accb80db7956461c3694
zackharshal <harshalgotarne25@gmail.com>
2024-06-20 13:23:27 +0530

    Add multiple contacts

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -31,6 +31,7 @@ public class AddressBook {
         System.out.println("Phone Number: "+ phoneNumber);
         System.out.println("Email: "+email);
     }
+    // created a new static function that deletes the contact
     static void deleteContact(ArrayList<AddressBook> contacts){
         Scanner scanner = new Scanner(System.in);
         for(int i=0;i<contacts.size();i++){
@@ -49,13 +50,13 @@ public class AddressBook {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Address Book program.");
         ArrayList<AddressBook> contacts = new ArrayList<AddressBook>();
-        boolean run = true;
-        while(run){
+        boolean condi = true;
+        while(condi){
             System.out.println("1. Add contact\t 2. Edit contact\t 3. Delete contact\t 4. Exit");
             int choice = scanner.nextInt();
             scanner.nextLine();
-            switch (choice){
-                case 1:
+            switch (choice) {
+                case 1 -> {
                     System.out.print("Enter the first name: ");
                     String firstName = scanner.nextLine();
                     System.out.print("Enter the last name: ");
@@ -72,17 +73,16 @@ public class AddressBook {
                     String phoneNumber = scanner.nextLine();
                     System.out.print("Enter the email: ");
                     String email = scanner.nextLine();
-                    contacts.add(new AddressBook(firstName,lastName,address,city,state,zipCode,phoneNumber,email));
-                    break;
-
-                case 2:
-                    for(AddressBook cont: contacts){
+                    contacts.add(new AddressBook(firstName, lastName, address, city, state, zipCode, phoneNumber, email));
+                }
+                case 2 -> {
+                    for (AddressBook cont : contacts) {
                         System.out.println(cont.firstName);
                     }
                     System.out.println("Enter the first name of that contact you want to edit: ");
                     String name = scanner.next();
-                    for (AddressBook cont: contacts) {
-                        if(Objects.equals(cont.firstName, name)){
+                    for (AddressBook cont : contacts) {
+                        if (Objects.equals(cont.firstName, name)) {
                             System.out.println("What do you want to edit?");
                             System.out.println("1. Address");
                             System.out.println("2. City");
@@ -91,59 +91,53 @@ public class AddressBook {
                             System.out.println("5. Phone number");
                             System.out.println("6. Email");
                             int scase = scanner.nextInt();
-                            switch (scase){
-                                case 1:
+                            switch (scase) {
+                                case 1 -> {
                                     scanner.nextLine();
                                     System.out.println("Enter the new address: ");
                                     cont.address = scanner.nextLine();
                                     System.out.println("The address has been changed.");
-                                    break;
-                                case 2:
+                                }
+                                case 2 -> {
                                     scanner.nextLine();
                                     System.out.println("Enter the new City: ");
                                     cont.city = scanner.nextLine();
                                     System.out.println("The City has been changed.");
-                                    break;
-                                case 3:
+                                }
+                                case 3 -> {
                                     scanner.nextLine();
                                     System.out.println("Enter the new State: ");
                                     cont.state = scanner.nextLine();
                                     System.out.println("The state has been changed.");
-                                    break;
-                                case 4:
+                                }
+                                case 4 -> {
                                     scanner.nextLine();
                                     System.out.println("Enter the new Zip code: ");
                                     cont.zipCode = scanner.nextLine();
                                     System.out.println("The zip code has been changed.");
-                                    break;
-                                case 5:
+                                }
+                                case 5 -> {
                                     scanner.nextLine();
                                     System.out.println("Enter the new phone number: ");
                                     cont.phoneNumber = scanner.nextLine();
                                     System.out.println("The phone number has been changed.");
-                                    break;
-                                case 6:
+                                }
+                                case 6 -> {
                                     scanner.nextLine();
                                     System.out.println("Enter the new email: ");
                                     cont.email = scanner.nextLine();
                                     System.out.println("The email has been changed.");
-                                    break;
-                                default:
-                                    System.out.println("Wrong number/key entered.");
+                                }
+                                default -> System.out.println("Wrong number/key entered.");
                             }
                             System.out.println("The new contact info is: ");
                             cont.printInfo();
                         }
                     }
-                    break;
-                case 3:
-                    deleteContact(contacts);
-                    break;
-                case 4:
-                    run = false;
-                    break;
-                default:
-                    System.out.println("Wrong number/key pressed.");
+                }
+                case 3 -> deleteContact(contacts);
+                case 4 -> condi = false;
+                default -> System.out.println("Wrong number/key pressed.");
             }
         }
     }
